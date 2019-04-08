14b874232db497e214c2512d8552404a7f6ba63c
luchangjiang <20207075@qq.com>
2019-04-08 15:30:32 +0800

    RateLimiterConfiguration

---
+++ river-gateway/src/main/java/com/cloud/river/gateway/config/RateLimiterConfiguration.java
@@ -0,0 +1,20 @@
+package com.cloud.river.gateway.config;
+
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Mono;
+
+/**
+ * @program: RiverCloud
+ * @description: 网关限流配置
+ * @author: River
+ * @create: 2019-04-08 15:27
+ **/
+@Configuration
+public class RateLimiterConfiguration {
+    @Bean(value ="remoteAddrKeyResolver")
+    public KeyResolver RemoteAddrKeyResolver(){
+        return exchange -> Mono.just(exchange.getRequest().getRemoteAddress().getAddress().getHostName());
+    }
+}
